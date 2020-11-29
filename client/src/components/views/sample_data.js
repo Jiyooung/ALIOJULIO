e34c2fdaea6a71b24e92620bd3018826466bba89
Seulwoo <gmgkglgh@naver.com>
2020-11-30 04:36:22 +0900

    appointmetns arrange:SW

---
+++ client/src/components/views/sample_data.js
@@ -0,0 +1,23 @@
+export const sample_data = [
+    {
+        title: "행사명",
+        startDate: new Date(2020,11,28,5,0),
+        endDate: new Date(2020,12,30,18,30),
+        id: 0,
+        location: "서울시 동작구"
+    },
+    {
+        title: "행사명222",
+        startDate: new Date(2020,11,30),
+        endDate: new Date(2020,11,30),
+        id: 1,
+        location: "서울시 동작구"
+    },
+    {
+        title: "행사명22233322",
+        startDate: "2020-11-30",
+        endDate: "2020-12-02",
+        id: 2,
+        location: "서울시 동작구"
+    },
+];
